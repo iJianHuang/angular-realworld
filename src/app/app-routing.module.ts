63cef9ac3da3869d32031338ed23ef1f659127f4
Jian Huang <iJianHuang@users.noreply.github.com>
2020-12-31 08:47:23 -0500

    refactor routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },  
   { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule) },
   { path: 'conduit', loadChildren: () => import('./conduit/conduit.module').then(m => m.ConduitModule) },
   { path: '**', redirectTo: 'home' }
